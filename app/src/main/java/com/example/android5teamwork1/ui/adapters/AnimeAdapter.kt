8ed3b263050c778811f9f02ee9ff0df1bda5ea73
Zununova <zununiazk@gmail.com>
2023-07-22 18:26:35 +0300

    connected adapter with recyclerView and created him

---
+++ app/src/main/java/com/example/android5teamwork1/ui/adapters/AnimeAdapter.kt
@@ -0,0 +1,59 @@
+package com.example.android5teamwork1.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.android5teamwork1.data.model.AnimeModel
+import com.example.android5teamwork1.databinding.ItemRecyclerViewBinding
+
+class AnimeAdapter :
+    ListAdapter<AnimeModel, AnimeAdapter.AnimeViewHolder>(DiffUtilCallback()) {
+
+    inner class AnimeViewHolder(private val binding: ItemRecyclerViewBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun onBind(animeModel: AnimeModel?) = with(binding) {
+            Glide.with(itemCharacterImage).load(animeModel?.image).into(itemCharacterImage)
+            itemCharacterName.text = animeModel?.title
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnimeViewHolder {
+        return AnimeViewHolder(
+            ItemRecyclerViewBinding.inflate(
+                LayoutInflater.from(
+                    parent.context
+                ), parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: AnimeViewHolder, position: Int) {
+        holder.onBind(getItem(position))
+    }
+
+    companion object {
+
+        class DiffUtilCallback : DiffUtil.ItemCallback<AnimeModel>() {
+
+            override fun areItemsTheSame(
+                oldItem: AnimeModel,
+                newItem: AnimeModel
+            ): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(
+                oldItem: AnimeModel,
+                newItem: AnimeModel
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
+
