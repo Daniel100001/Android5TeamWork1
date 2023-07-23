a6b65fa7b30286d293fdb9aaf6dddfa733594a40
Daniel100001 <danldplayer2gmail.com>
2023-07-23 17:46:24 +0600

    made transition to DetailFragment using onItemClick and displayed data there.

--- app/src/main/java/com/example/android5teamwork1/ui/fragments/AnimeDetailFragment.kt
+++ app/src/main/java/com/example/android5teamwork1/ui/fragments/AnimeDetailFragment.kt
@@ -1,7 +1,11 @@
 package com.example.android5teamwork1.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.bumptech.glide.Glide
 import com.example.android5teamwork1.R
 import com.example.android5teamwork1.base.BaseFragment
 import com.example.android5teamwork1.databinding.FragmentAnimeDetailFragmnetBinding
@@ -10,4 +14,16 @@ class AnimeDetailFragment :
     BaseFragment<FragmentAnimeDetailFragmnetBinding, AnimeViewModel>(R.layout.anime_fragment) {
     override val binding by viewBinding(FragmentAnimeDetailFragmnetBinding::bind)
     override val viewModel: AnimeViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setData()
+    }
+
+    private fun setData() {
+        val args by navArgs<AnimeDetailFragmentArgs>()
+        binding.textViewDetail.text = args.model.detail
+        Glide.with(binding.imageViewPoster).load(args.model.image)
+            .into(binding.imageViewPoster)
+    }
 }
